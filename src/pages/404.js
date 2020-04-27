54343fa3a7c0a5469ff42e6d9418afc4cbf03c04
Jungho Son <js3692@users.noreply.github.com>
2020-04-27 00:36:37 -0400

    Replace 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,71 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import { css } from '@emotion/core'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import { ErrorLayout } from '@components/layouts'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <ErrorLayout>
+      <section
+        css={(theme) => css`
+          padding-bottom: 10vw;
+          border-bottom: 1px solid ${theme.colors.lightgrey_a};
+          text-align: center;
+
+          @media (max-width: 800px) {
+            padding-bottom: 16vw;
+          }
+
+          @media (max-width: 500px) {
+            padding-bottom: 14vw;
+          }
+        `}
+      >
+        <h1
+          css={(theme) => css`
+            margin: 0;
+            color: ${theme.colors.lightgrey};
+            font-size: 12vw;
+            line-height: 1em;
+            letter-spacing: -5px;
+            opacity: 0.75;
+
+            @media (max-width: 800px) {
+              font-size: 11.2rem;
+            }
+          `}
+        >
+          404
+        </h1>
+        <p
+          css={(theme) => css`
+            margin: 0;
+            color: ${theme.colors.midgrey};
+            font-size: 3rem;
+            line-height: 1.3em;
+            font-weight: 400;
+
+            @media (max-width: 800px) {
+              margin: 5px 0 0 0;
+              font-size: 1.8rem;
+            }
+          `}
+        >
+          Page not found
+        </p>
+        <Link
+          to="/"
+          css={css`
+            display: inline-block;
+            margin-top: 5px;
+          `}
+        >
+          Go to the front page →
+        </Link>
+      </section>
+    </ErrorLayout>
+  )
+}
 
 export default NotFoundPage
