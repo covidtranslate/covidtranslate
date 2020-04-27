6cbf3bdd20880de0a3562ab5c7fb1743f70f6e3e
Jungho Son <js3692@users.noreply.github.com>
2020-04-26 21:21:51 -0400

    Add cta link

---
+++ src/components/common/LinkCTA.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import { css } from '@emotion/core'
+import { ArrowRight, ExternalLink, Download } from 'react-feather'
+
+const EXTERNAL_LINK = /^http/
+const FILE_LINK = /\.(?:pdf|xlsx)$/
+
+export const LinkCTA = ({ text, link }) => {
+  let LinkComponent
+  let LinkIcon
+  let linkProps
+
+  if (EXTERNAL_LINK.test(link)) {
+    LinkComponent = OutboundLink
+    LinkIcon = ExternalLink
+    linkProps = { href: link }
+  } else if (FILE_LINK.test(link)) {
+    LinkComponent = OutboundLink
+    LinkIcon = Download
+    linkProps = { href: link }
+  } else {
+    LinkComponent = Link
+    LinkIcon = ArrowRight
+    linkProps = { to: link }
+  }
+
+  return (
+    <p
+      css={(theme) => css`
+        display: flex;
+        align-items: center;
+        & a {
+          color: ${theme.colors.blue};
+          font-size: 24px;
+          text-decoration: underline;
+          box-shadow: none;
+          transition: all 0.2s ease-in-out;
+        }
+        & a:hover {
+          color: ${theme.colors.blue_c};
+          text-decoration: underline;
+          box-shadow: none;
+        }
+      `}
+    >
+      <LinkComponent {...linkProps}>
+        <b>{text}</b>
+        <LinkIcon
+          size={18}
+          css={(theme) => css`
+            margin-left: 8px;
+            margin-bottom: 4px;
+            color: ${theme.colors.darkgrey};
+          `}
+        />
+      </LinkComponent>
+    </p>
+  )
+}
+
+LinkCTA.propTypes = {
+  text: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+}
