36d48da681d507e3ddffcb60ecc47473ffbeac0c
Jungho Son <js3692@users.noreply.github.com>
2020-05-07 23:21:02 -0400

    Remove offline package

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    'gatsby-plugin-remove-serviceworker',
     'gatsby-plugin-emotion',
     {
       resolve: 'gatsby-plugin-google-analytics',
